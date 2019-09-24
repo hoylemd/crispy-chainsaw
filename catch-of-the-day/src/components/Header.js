8ade78aa23c5b045dd25290b1270fc9d132ea101
Michael D. Hoyle <hoylemd@gmail.com>
2019-09-24 17:17:33 -0400

    Implement the header for the list

--- catch-of-the-day/src/components/Header.js
+++ catch-of-the-day/src/components/Header.js
@@ -4,7 +4,19 @@ import React from 'react';
 class Header extends React.Component {
   render() {
     return (
-      <p>Header component</p>
+      <header className="top">
+        <h1>
+          Catch
+          <span className="ofThe">
+            <span className="of">Of</span>
+            <span className="the">The</span>
+          </span>
+          Day
+        </h1>
+        <h3 className="tagline">
+          <span>Fresh Daily</span>
+        </h3>
+      </header>
     );
   }
 }
