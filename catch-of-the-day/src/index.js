a59652a6de3a6f3f23e6905b3353880542a4ffdc
Michael D. Hoyle <hoylemd@gmail.com>
2019-09-24 13:38:21 -0400

    import css

--- catch-of-the-day/src/index.js
+++ catch-of-the-day/src/index.js
@@ -1,6 +1,13 @@
+// library imports
 import React from 'react';
 import { render } from 'react-dom';
 
+// component imports
 import StorePicker from './components/StorePicker';
+
+// style imports
+import './css/style.css';
+
+
 
 render(<StorePicker/>, document.querySelector('#main'));
