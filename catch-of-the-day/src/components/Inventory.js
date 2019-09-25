8b3ab64c1d97c6fa8fb35cc818d9b5c57effa9f5
Michael D. Hoyle <hoylemd@gmail.com>
2019-09-25 13:38:04 -0400

    some small tweaks

--- catch-of-the-day/src/components/Inventory.js
+++ catch-of-the-day/src/components/Inventory.js
@@ -1,7 +1,6 @@
 import React from 'react';
-//import PropTypes from 'prop-types';
 
-const Inventory = (props) => (
+const Inventory = () => (
   <div className="inventory">
     Inventory!
   </div>
