9d1b1966be01d18820d726da15b48e7d266af0c9
Michael D. Hoyle <hoylemd@gmail.com>
2019-09-26 17:14:32 -0400

    Add fixture loading

--- catch-of-the-day/src/components/App.js
+++ catch-of-the-day/src/components/App.js
@@ -6,6 +6,8 @@ import Header from './Header';
 import Order from './Order';
 import Inventory from './Inventory';
 
+import sampleFishes from '../sample-fishes';
+
 class App extends React.Component {
   state = {
     fishes: {},
@@ -18,6 +20,12 @@ class App extends React.Component {
     this.setState({fishes: fishes});
   };
 
+  loadSampleFishes = () => {
+    this.setState({
+      fishes: sampleFishes
+    });
+  };
+
   render() {
     return (
       <div className="catch-of-the-day">
@@ -26,7 +34,7 @@ class App extends React.Component {
           <Header tagline="Fresh Seafood Market"/>
         </div>
         <Order />
-        <Inventory addFish={this.addFish}/>
+        <Inventory addFish={this.addFish} loadSampleFishes={this.loadSampleFishes}/>
       </div>
     );
   }
