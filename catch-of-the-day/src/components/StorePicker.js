a6524bdcc13ecb95fbccb09391a55cd6d427aefe
Michael D. Hoyle <hoylemd@gmail.com>
2019-09-26 11:47:49 -0400

    Handle the click event in the storepicker

--- catch-of-the-day/src/components/StorePicker.js
+++ catch-of-the-day/src/components/StorePicker.js
@@ -3,15 +3,25 @@ import React from 'react';
 // local imports
 import { getFunName } from '../helpers';
 
-const StorePicker = () => (
-  <form className="store-picker">
-    <h2>Enter the name for your store</h2>
-    <input type="text" required
-      placeholder="Store Name"
-      defaultValue={getFunName()}
-    />
-    <button type="Visit Store">Go</button>
-  </form>
-);
+class StorePicker extends React.Component {
+  handleSubmit(event) {
+    // stop the submittion request
+    event.preventDefault();
+    alert('to the store!');
+  }
+
+  render() {
+    return (
+      <form className="store-picker" onSubmit={this.handleSubmit}>
+        <h2>Enter the name for your store</h2>
+        <input type="text" required
+          placeholder="Store Name"
+          defaultValue={getFunName()}
+        />
+        <button type="submit">Go</button>
+      </form>
+    );
+  }
+}
 
 export default StorePicker;
