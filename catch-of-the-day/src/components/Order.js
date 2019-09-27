fdcb3255fc29b82fc5294b9e70a1036a0de4d0ea
Michael D. Hoyle <hoylemd@gmail.com>
2019-09-27 12:15:29 -0400

    Display order

--- catch-of-the-day/src/components/Order.js
+++ catch-of-the-day/src/components/Order.js
@@ -1,10 +1,25 @@
 import React from 'react';
 //import PropTypes from 'prop-types';
 
-const Order = (props) => (
-  <div className="order">
-    Ordaaaahhhh!
-  </div>
-);
+class Order extends React.Component {
+  render () {
+    const order_items = Object.keys(this.props.order).map(key =>
+      <li key={'o' + key}>
+        {this.props.fishes[key].name} ({this.props.order[key]} lbs)
+      </li>
+    );
+    return (
+      <div className="order-wrap">
+        <h2>Your Order</h2>
+        <ul>
+          {order_items}
+        </ul>
+        <div className="order-total">
+
+        </div>
+      </div>
+    );
+  }
+}
 
 export default Order;
