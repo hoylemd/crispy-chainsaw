6327b65daf5a0cfc69b38c4cb643c1bedb81b790
Michael D. Hoyle <hoylemd@gmail.com>
2019-10-07 12:00:19 -0400

    remove fb creds

--- catch-of-the-day/src/base.js
+++ catch-of-the-day/src/base.js
@@ -2,13 +2,7 @@ import Rebase from 're-base';
 import firebase from 'firebase';
 
 const firebaseApp = firebase.initializeApp({
-  apiKey: 'AIzaSyD4cV6RzODC-guI9fX0YVZIV3JFUhkVI7M',
-  authDomain: 'catch-of-the-day-a2440.firebaseapp.com',
-  databaseURL: 'https://catch-of-the-day-a2440.firebaseio.com',
-  projectId: 'catch-of-the-day-a2440',
-  storageBucket: 'catch-of-the-day-a2440.appspot.com',
-  messagingSenderId: '806447017437',
-  appId: '1:806447017437:web:fe4a2683ad6999ec679a34'
+  // paste firebase config here
 });
 
 const base = Rebase.createClass(firebaseApp.database());
